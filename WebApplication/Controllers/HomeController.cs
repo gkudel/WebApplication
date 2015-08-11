699647a1f2015a78ed0906f3f95c48eeed3fe0c2
unknown <gkudlacz@GKUDLACZ-RZE.pgs-soft.com>
2015-08-11 13:51:12 +0200

    sss

--- WebApplication/Controllers/HomeController.cs
+++ WebApplication/Controllers/HomeController.cs
@@ -12,5 +12,10 @@ namespace WebApplication.Controllers
         {
             return View();
         }
+
+        public ActionResult Home()
+        {
+            return View();
+        }
     }
 }
