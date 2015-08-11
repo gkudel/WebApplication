e3c49a755932dc4ad99c5b2abed3a5d6f56d2b28
unknown <mkrzeminski@MKRZEMINSKI-RZE.pgs-soft.com>
2015-08-11 13:47:49 +0200

    Conflicting changes

--- WebApplication/Controllers/HomeController.cs
+++ WebApplication/Controllers/HomeController.cs
@@ -8,7 +8,15 @@ namespace WebApplication.Controllers
 {
     public class HomeController : Controller
     {
+        public ActionResult TestOne()
+        {
+            return View();
+        }
         public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult TestTwo()
         {
             return View();
         }
